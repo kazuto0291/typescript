0bf16decc90ce40ffa2678f147022c2374b865db
kazuto0291 <zutozuto03170291@gmail.com>
2020-04-30 18:39:03 +0900

    型

--- TYPESCRIPT-TYPE/index.ts
+++ TYPESCRIPT-TYPE/index.ts
@@ -1,59 +1,41 @@
-let hasValue= true;
+// boolean型,number型,string型
+// let hasValue: boolean =true;
+// let count: number = 10;
+// let float: number = 3.14;
+// let negative: number = -0.12;
+// let single: string = 'hello';
+// let double: string = "hello";
+// let back: string = `hello`;
+
+// 型推論  基本癖に使う,型推論が出来ないときに方注釈を使う
+let hasValue =true;
 let count = 10;
-let float =3.14;
+let float = 3.14;
 let negative = -0.12;
 let single = 'hello';
 let double = "hello";
 let back = `hello`;
-let hello: string;
-hello = 'hello';
 
-const person = {
+// オブジェクトに型をつける
+const person: {
+  name: string;
+  age: number;
+} = {
   name: {
-    first: 'jack',
-    last: 'smith'
+    first: 'Jack',
+    last: 'Smith'
   },
   age: 21
 }
+console.log(person.age)
 
-const fruits = ['apple', 'banana', 'grape']
 
+// 配列にかたをつけるarray型
+const fruits: string[] = ['Apple', 'Banana', 'Grape']
 const fruit = fruits[0];
 
+// Tuple型使用して、決まった内容の配列を作る(tuple型は型推論をしない)
 const book: [string, number, boolean] = ['business', 1500, false];
-
-enum CoffeeSize {
-  SHORT= 'SHORT',
-  TALL= 'TALLL',
-  GRANDE= 'GRANDE',
-  VENTI= 'VENTI'
-}
-const coffee = {
-  hot: true,
-  size: CoffeeSize.TALL
-}
-
-coffee.size = CoffeeSize.SHORT;
-
-let anything: any = true;
-anything = 'hello';
-anything = ['hello', 33, true];
-anything = {};
-anything.fjiafjaj = 'fjaeife';
-let banana = 'banana';
-banana = anything;
-
-let unionType: number | string = 10;
-let unionTypes: (number | string) [] = [21, 'hello']
+// book[1] = ture;
 
 
-type ClothSize = 'small' | 'medium' | 'large'
-const apple = 'apple';
-let clothSize: ClothSize = 'large'
-const cloth: {
-  color: string;
-  size: ClothSize
-} = {
-  color: 'white',
-  size: 'medium'
-}
