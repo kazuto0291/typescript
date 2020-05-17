f9a71cccad1206c9454d56a822d371a24dd96a4e
kazuto0291 <zutozuto03170291@gmail.com>
2020-05-18 02:44:38 +0900

    void型
    関数が何も返さないときに使う
    
    undefined型は使わない

--- TYPESCRIPT-TYPE/index.js
+++ TYPESCRIPT-TYPE/index.js
@@ -6,16 +6,14 @@
 // let single: string = 'hello';
 // let double: string = "hello";
 // let back: string = `hello`;
-
 // 型推論  基本癖に使う,型推論が出来ないときに方注釈を使う
-let hasValue =true;
-let count = 10;
-let float = 3.14;
-let negative = -0.12;
-let single = 'hello';
-let double = "hello";
-let back = `hello`;
-
+var hasValue = true;
+var count = 10;
+var float = 3.14;
+var negative = -0.12;
+var single = 'hello';
+var double = "hello";
+var back = "hello";
 // オブジェクトに型をつける
 // const person: {
 //   name: string;
@@ -28,17 +26,12 @@ let back = `hello`;
 //   age: 21
 // }
 // console.log(person.age)
-
-
 // 配列にかたをつけるarray型
-const fruits: string[] = ['Apple', 'Banana', 'Grape']
-const fruit = fruits[0];
-
+var fruits = ['Apple', 'Banana', 'Grape'];
+var fruit = fruits[0];
 // Tuple型使用して、決まった内容の配列を作る(tuple型は型推論をしない)
-const book: [string, number, boolean] = ['business', 1500, false];
+var book = ['business', 1500, false];
 // book[1] = ture;
-
-
 // Enumを使って、特定のまとまったグループのみを受け入れる列挙型を使う方法
 // Enumをしてするときは全部大文字が主流、先頭の変数名も大文字が一般的
 // const CoffeeSize = {
@@ -56,59 +49,43 @@ const book: [string, number, boolean] = ['business', 1500, false];
 //   VENTI = 'VENTI'
 // }
 //＝を省略できる
-enum CoffeeSize  {
-  SHORT,
-  TALL,
-  GRANDE,
-  VENTI
-}
-const coffee = {
-  hot: true,
-  size: CoffeeSize.TALL
-}
-
-coffee.size =CoffeeSize.SHORT;
-
+var CoffeeSize;
+(function (CoffeeSize) {
+    CoffeeSize[CoffeeSize["SHORT"] = 0] = "SHORT";
+    CoffeeSize[CoffeeSize["TALL"] = 1] = "TALL";
+    CoffeeSize[CoffeeSize["GRANDE"] = 2] = "GRANDE";
+    CoffeeSize[CoffeeSize["VENTI"] = 3] = "VENTI";
+})(CoffeeSize || (CoffeeSize = {}));
+var coffee = {
+    hot: true,
+    size: CoffeeSize.TALL
+};
+coffee.size = CoffeeSize.SHORT;
 //どんな方も入るany型
 //とてもカオスなので使わないようにする
 // JS→TSに以降の時有り
-let anything: any = true;
+var anything = true;
 anything = 'hello';
-anything  = ['hello', 33, true];
+anything = ['hello', 33, true];
 anything = {};
 anything.fjiafja = 'faifai';
-
 // Union型を使って複数の型を使う方法
 //したはnumberもstringも扱えるよの意味
-let unionType:number | string = 10;
-let unionTypes: (number | string)[] = [21, 'hello']
-
+var unionType = 10;
+var unionTypes = [21, 'hello'];
 // Literal型を使って複雑な型を変数のように扱う
 // リテラル型は特定の決まった値のみを扱う型
 // constを使うとリテラル型になる
-const apple: 'apple' = 'apple';
-const mikan: 0 = 0;
-
-// リテラル型とユニオン型を組み合わせるとEnum型に似たのが作れる
-// 基本的にはEunm型の方が良いが候補が２〜３個のときは使ってもいい
-type ClothSize = 'small' | 'medium' | 'large'
-let clothSize: ClothSize = 'medium';
-const cloth: {
-  color: string;
-  size: ClothSize
-} = {
-  color: 'white',
-  size: clothSize
-}
-cloth.size = "small"
-
+var apple = 'apple';
+var mikan = 0;
+var clothSize = 'medium';
+var cloth = {
+    color: 'white',
+    size: clothSize
+};
+cloth.size = "small";
 // 関数に型
-
-function add(num1: number, num2: number): number {
-  return num1 + num2
+function add(num1, num2) {
+    return num1 + num2;
 }
 
-// 関数が何も返さないときvoid型を使う
-function sayHello(): void {
-  console.log('Hello!');
-}
