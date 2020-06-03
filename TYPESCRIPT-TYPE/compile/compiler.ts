acc1122f36643897d66c26c0c326312c774c39d7
kazuto0291 <zutozuto03170291@gmail.com>
2020-06-03 19:20:49 +0900

    strictの設定７つの確認

--- TYPESCRIPT-TYPE/compile/compiler.ts
+++ TYPESCRIPT-TYPE/compile/compiler.ts
@@ -3,3 +3,21 @@ console.log('Hello');
 
 let hi ='he';
 console.log(hi.toUpperCase())
+
+function echo(message:string): string | null {
+  return message;
+}
+
+let impliciAny;
+impliciAny = 'implicitAny';
+
+let nullableMessage: string | null = echo('hi');
+let undefindableMessage: string | undefined = undefined;
+let onlyNUll: null = null;
+let onlyUNdefined: undefined =undefined
+
+if (nullableMessage) {
+  nullableMessage.toUpperCase();
+}
+
+const newFunction = echo.call(null, "hi")
