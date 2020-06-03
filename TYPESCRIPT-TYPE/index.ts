f7fd117a845218297d126f25849945759a5ff1d8
kazuto0291 <zutozuto03170291@gmail.com>
2020-06-03 16:54:04 +0900

    never型
    決して何も返さない型

--- TYPESCRIPT-TYPE/index.ts
+++ TYPESCRIPT-TYPE/index.ts
@@ -8,13 +8,13 @@
 // let back: string = `hello`;
 
 // 型推論  基本癖に使う,型推論が出来ないときに方注釈を使う
-let hasValue =true;
-let count = 10;
-let float = 3.14;
-let negative = -0.12;
-let single = 'hello';
-let double = "hello";
-let back = `hello`;
+// let hasValue =true;
+// let count = 10;
+// let float = 3.14;
+// let negative = -0.12;
+// let single = 'hello';
+// let double = "hello";
+// let back = `hello`;
 
 // オブジェクトに型をつける
 // const person: {
@@ -31,11 +31,11 @@ let back = `hello`;
 
 
 // 配列にかたをつけるarray型
-const fruits: string[] = ['Apple', 'Banana', 'Grape']
-const fruit = fruits[0];
+// const fruits: string[] = ['Apple', 'Banana', 'Grape']
+// const fruit = fruits[0];
 
 // Tuple型使用して、決まった内容の配列を作る(tuple型は型推論をしない)
-const book: [string, number, boolean] = ['business', 1500, false];
+// const book: [string, number, boolean] = ['business', 1500, false];
 // book[1] = ture;
 
 
@@ -56,64 +56,64 @@ const book: [string, number, boolean] = ['business', 1500, false];
 //   VENTI = 'VENTI'
 // }
 //＝を省略できる
-enum CoffeeSize  {
-  SHORT,
-  TALL,
-  GRANDE,
-  VENTI
-}
-const coffee = {
-  hot: true,
-  size: CoffeeSize.TALL
-}
+// enum CoffeeSize  {
+//   SHORT,
+//   TALL,
+//   GRANDE,
+//   VENTI
+// }
+// const coffee = {
+//   hot: true,
+//   size: CoffeeSize.TALL
+// }
 
-coffee.size =CoffeeSize.SHORT;
+// coffee.size =CoffeeSize.SHORT;
 
 //どんな方も入るany型
 //とてもカオスなので使わないようにする
 // JS→TSに以降の時有り
-let anything: any = true;
-anything = 'hello';
-anything  = ['hello', 33, true];
-anything = {};
-anything.fjiafja = 'faifai';
+// let anything: any = true;
+// anything = 'hello';
+// anything  = ['hello', 33, true];
+// anything = {};
+// anything.fjiafja = 'faifai';
 
 // Union型を使って複数の型を使う方法
 //したはnumberもstringも扱えるよの意味
-let unionType:number | string = 10;
-let unionTypes: (number | string)[] = [21, 'hello']
+// let unionType:number | string = 10;
+// let unionTypes: (number | string)[] = [21, 'hello']
 
 // Literal型を使って複雑な型を変数のように扱う
 // リテラル型は特定の決まった値のみを扱う型
 // constを使うとリテラル型になる
-const apple: 'apple' = 'apple';
-const mikan: 0 = 0;
+// const apple: 'apple' = 'apple';
+// const mikan: 0 = 0;
 
 // リテラル型とユニオン型を組み合わせるとEnum型に似たのが作れる
 // 基本的にはEunm型の方が良いが候補が２〜３個のときは使ってもいい
-type ClothSize = 'small' | 'medium' | 'large'
-let clothSize: ClothSize = 'medium';
-const cloth: {
-  color: string;
-  size: ClothSize
-} = {
-  color: 'white',
-  size: clothSize
-}
-cloth.size = "small"
+// type ClothSize = 'small' | 'medium' | 'large'
+// let clothSize: ClothSize = 'medium';
+// const cloth: {
+//   color: string;
+//   size: ClothSize
+// } = {
+//   color: 'white',
+//   size: clothSize
+// }
+// cloth.size = "small"
 
 // 関数に型
 
-function add(num1: number, num2: number): number {
-  return num1 + num2
-}
+// function add(num1: number, num2: number): number {
+//   return num1 + num2
+// }
 
 // 関数が何も返さないときvoid型を使う
-function sayHello(): void {
-  console.log('Hello!');
-}
+// function sayHello(): void {
+  // console.log('Hello!');
+// }
 
-console.log(sayHello());
+// console.log(sayHello());
 let tmp: undefined =  null;
 let tmpNUll: null  =undefined;
 
@@ -129,7 +129,7 @@ const doubleNumber1: (num: number) =>number = num=> num * 2;
 // コールバック関数に型をつける
 function doubleAndHNdle(num: number, cb: (num:number) => number): void {
   const doubleNum = cb(num * 2);
-  console.log(doubleNum);
+  // console.log(doubleNum);
 }
 
 doubleAndHNdle(21, doubleNum => {
@@ -149,3 +149,9 @@ text = anyInput;
 if (typeof unknownInput === 'string') {
   text = unknownInput;
 }
+
+// never型。。決して何も返さない型
+function error(message: string):never {
+  throw new Error(message);
+}
+console.log(error('this is an error'));
