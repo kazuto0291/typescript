c669ab50bfa03c720237146ea9b1fed7233313af
kazuto0291 <zutozuto03170291@gmail.com>
2020-06-09 14:51:20 +0900

    readonly修飾子
    変わらないものをreadonlyにすることによって、不意に変更できなくする。privateとの違いは、privataはクラスの中で読み書きできるが、readonlyは、クラスの中と外で読むことしかできない、書き換えることができない。constructorのなかは書き換えできる。

--- TYPESCRIPT-TYPE/class/index.ts
+++ TYPESCRIPT-TYPE/class/index.ts
@@ -1,5 +1,7 @@
 class Person {
-  constructor(public name: string, private age: number) {
+  readonly id: number = 32;
+  constructor(public readonly name: string, private age: number) {
+    
   }
   incrementAge() {
     this.age += 1;
