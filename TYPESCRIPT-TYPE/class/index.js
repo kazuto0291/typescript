5195619521d95f707095442d0d9c02295a66991e
kazuto0291 <zutozuto03170291@gmail.com>
2020-06-09 14:37:34 +0900

    private修飾子を使用してアクセスを制限する方法
    関数を通してのみ変更するときに使う。外から読み込みもできない。

--- TYPESCRIPT-TYPE/class/index.js
+++ TYPESCRIPT-TYPE/class/index.js
@@ -1,17 +1,17 @@
 var Person = /** @class */ (function () {
-    function Person(initName) {
+    function Person(initName, initAge) {
         this.name = initName;
+        this.age = initAge;
     }
+    Person.prototype.incrementAge = function () {
+        this.age += 1;
+    };
     Person.prototype.greeting = function () {
-        console.log("Hello! My name is " + this.name);
+        console.log("Hello! My name is " + this.name + ". I am " + this.age + " years old");
     };
     return Person;
 }());
-var quill = new Person('Quill');
-console.log(quill);
+var person2;
+var quill = new Person('Quill', 38);
 quill.greeting();
-var anotherQuill = {
-    name: 'anotherQuill',
-    anotehrGreeting: quill.greeting
-};
-anotherQuill.anotehrGreeting();
+quill.incrementAge();
--- TYPESCRIPT-TYPE/class/index.ts
+++ TYPESCRIPT-TYPE/class/index.ts
@@ -1,25 +1,21 @@
 class Person {
   name: string;
-  constructor(initName: string) {
+  private age: number;
+  constructor(initName: string, initAge: number) {
     this.name = initName;
+    this.age = initAge;
   }
-
+  incrementAge() {
+    this.age += 1;
+  }
   greeting(this: Person) {
-    console.log(`Hello! My name is ${this.name}`)
+    console.log(`Hello! My name is ${this.name}. I am ${this.age} years old`)
   }
 }
 
 let person2: Person;
 
-const quill = new Person('Quill');
-console.log(quill);
-
+const quill = new Person('Quill', 38);
 quill.greeting();
+quill.incrementAge();
 
-const anotherQuill = {
-  name: 'anotherQuill',
-  greeting() {},
-  anotehrGreeting: quill.greeting
-}
-
-anotherQuill.anotehrGreeting();
