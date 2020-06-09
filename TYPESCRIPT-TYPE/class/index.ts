ace286806af02e51d0765257179d990f80d9df33
kazuto0291 <zutozuto03170291@gmail.com>
2020-06-09 14:27:06 +0900

    TypeScriptは型を作り出す時、型も作り出している。
    TypeScriptはクラスを作る（オブジェクトを作る、インスタンスを作る）機能以外に、そのクラスが作り出すインスタンスを表す型も同時に作っている

--- TYPESCRIPT-TYPE/class/index.ts
+++ TYPESCRIPT-TYPE/class/index.ts
@@ -4,10 +4,12 @@ class Person {
     this.name = initName;
   }
 
-  greeting(this: {name: string}) {
+  greeting(this: Person) {
     console.log(`Hello! My name is ${this.name}`)
   }
 }
+
+let person2: Person;
 
 const quill = new Person('Quill');
 console.log(quill);
@@ -16,6 +18,7 @@ quill.greeting();
 
 const anotherQuill = {
   name: 'anotherQuill',
+  greeting() {},
   anotehrGreeting: quill.greeting
 }
 
