83fd83ecc57486f89a80427f7f41012c9670245b
kazuto0291 <zutozuto03170291@gmail.com>
2020-12-11 03:00:23 +0900

    interface関数の型

--- TYPESCRIPT-TYPE/interface/interface.ts
+++ TYPESCRIPT-TYPE/interface/interface.ts
@@ -1,11 +1,15 @@
 interface Human  {
   name: string;
   age: number;
+  greeting(message: string): void;
 }
 
 const human: Human = {
   name: 'Quill',
-  age: 38
+  age: 38,
+  greeting(message: string) {
+    console.log(message)
+  }
 }
 
 let developer: Human;
